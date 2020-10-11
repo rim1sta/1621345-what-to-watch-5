5d29be8b62278858bd2e5653a5a3fed637f6c1ba
Konstantin <rim1sta@MacBook-Pro-Konstantin.local>
2020-10-11 23:30:26 +0300

    add router, create new components

---
+++ src/components/add-rewiew/add-rewiew.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+
+const AddRewiew = () => {
+  return (
+    <React.Fragment>
+
+      <div className="user-block">
+        <div className="user-block__avatar">
+          <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+        </div>
+      </div>
+
+      <div className="movie-card__poster movie-card__poster--small">
+        <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+      </div>
+
+      <div className="add-review">
+        <form action="#" className="add-review__form">
+          <div className="rating">
+            <div className="rating__stars">
+              <input className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
+              <label className="rating__label" htmlFor="star-1">Rating 1</label>
+
+              <input className="rating__input" id="star-2" type="radio" name="rating" value="2" />
+              <label className="rating__label" htmlFor="star-2">Rating 2</label>
+
+              <input className="rating__input" id="star-3" type="radio" name="rating" value="3" checked />
+              <label className="rating__label" htmlFor="star-3">Rating 3</label>
+
+              <input className="rating__input" id="star-4" type="radio" name="rating" value="4" />
+              <label className="rating__label" htmlFor="star-4">Rating 4</label>
+
+              <input className="rating__input" id="star-5" type="radio" name="rating" value="5" />
+              <label className="rating__label" htmlFor="star-5">Rating 5</label>
+            </div>
+          </div>
+
+          <div className="add-review__text">
+            <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+            <div className="add-review__submit">
+              <button className="add-review__btn" type="submit">Post</button>
+            </div>
+
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default AddRewiew;
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MainPage from "../main-page/main-page";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Login from "../login/login";
+import MyList from "../my-list/my-list";
+import Film from "../film/film";
+import AddRewiew from "../add-rewiew/add-rewiew";
+import Player from "../player/player";
 
 const App = (props) => {
   const {filmTitle, filmGenre, filmYear} = props;
   return (
-    <MainPage
-      filmTitle={filmTitle}
-      filmGenre={filmGenre}
-      filmYear={filmYear} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage
+            filmTitle={filmTitle}
+            filmGenre={filmGenre}
+            filmYear={filmYear} />
+        </Route>
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+        <Route exact path="/mylist">
+          <MyList/>
+        </Route>
+        <Route exact path="/films/:id">
+          <Film/>
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddRewiew/>
+        </Route>
+        <Route exact path="/player/:id">
+          <Player/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
