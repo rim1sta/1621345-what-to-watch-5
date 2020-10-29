b7fc11fdca869c6a702b284ecad7e16445ed2e59
Konstantin <rim1sta@mail.ru>
2020-10-29 15:49:43 +0300

    create mocks

---
+++ src/mocks/films.js
@@ -0,0 +1,162 @@
+export const films = [
+  {
+    id: `1`,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    genre: `crime`,
+    year: 2014,
+    director: `Wes Andreson`,
+    duration: 124,
+    starring:
+    [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+    ],
+    description: `In the 1930s, the Grand Budapest `,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    previewVideo: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  },
+  {
+    id: `2`,
+    title: `The Grand Budapest Hotel`,
+    genre: `crime`,
+    year: 2014,
+    director: `Wes Andreson`,
+    duration: 124,
+    starring:
+    [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+    ],
+    description: `In the 1930s, the Grand Budapest `,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    previewVideo: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  },
+  {
+    id: `3`,
+    title: `Bohemian Rhapsody`,
+    genre: `crime`,
+    year: 2014,
+    director: `Wes Andreson`,
+    duration: 124,
+    starring:
+    [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+    ],
+    description: `In the 1930s, the Grand Budapest `,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    previewVideo: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  },
+  {
+    id: `4`,
+    title: `Macbeth`,
+    genre: `crime`,
+    year: 2014,
+    director: `Wes Andreson`,
+    duration: 124,
+    starring:
+    [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+    ],
+    description: `In the 1930s, the Grand Budapest `,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    previewVideo: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  },
+  {
+    id: `5`,
+    title: `Aviator`,
+    genre: `crime`,
+    year: 2014,
+    director: `Wes Andreson`,
+    duration: 124,
+    starring:
+    [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+    ],
+    description: `In the 1930s, the Grand Budapest `,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    previewVideo: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  },
+  {
+    id: `6`,
+    title: `What We Do in the Shadows`,
+    genre: `crime`,
+    year: 2014,
+    director: `Wes Andreson`,
+    duration: 124,
+    starring:
+    [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+    ],
+    description: `In the 1930s, the Grand Budapest `,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    previewVideo: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  },
+  {
+    id: `7`,
+    title: `Revenant`,
+    genre: `crime`,
+    year: 2014,
+    director: `Wes Andreson`,
+    duration: 124,
+    starring:
+    [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+    ],
+    description: `In the 1930s, the Grand Budapest `,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    previewVideo: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  },
+  {
+    id: `8`,
+    title: `Johnny English`,
+    genre: `crime`,
+    year: 2014,
+    director: `Wes Andreson`,
+    duration: 124,
+    starring:
+    [
+      `Bill Murray`,
+      `Edward Norton`,
+      `Jude Law`,
+      `Willem Dafoe`,
+      `Saoirse Ronan`,
+    ],
+    description: `In the 1930s, the Grand Budapest `,
+    poster: `img/the-grand-budapest-hotel-poster.jpg`,
+    video: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+    previewVideo: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
+  },
+]
