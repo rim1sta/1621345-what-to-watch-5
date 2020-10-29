9edf02b936e8029a62be53cc0f908e54ce0b6d2b
Konstantin <rim1sta@mail.ru>
2020-10-29 16:04:31 +0300

    connect mocks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
+import films from "./mocks/films";
+import PropTypes for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+
+}
+
 
 const movieInfo = {
   filmTitle: `The Grand Budapest Hotel`,
@@ -13,6 +19,7 @@ ReactDOM.render(
       filmTitle={movieInfo.filmTitle}
       filmGenre={movieInfo.filmGenre}
       filmYear={movieInfo.filmYear}
+      films={films}
     />,
     document.querySelector(`#root`)
 );
